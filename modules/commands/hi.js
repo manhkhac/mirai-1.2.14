9bfd8690001c3906a585f432cb7992cbe6488b5c
demngayyeu <nguyenmanhict@gmail.com>
2021-08-18 02:36:04 +0700

    update hi

--- modules/commands/hi.js
+++ modules/commands/hi.js
@@ -8,36 +8,40 @@ module.exports.config = {
     usages: "",
     cooldowns: 0,
     denpendencies: {
-        "fs": "",
-        "request": ""
+      "fs": "",
+      "request": ""
     }
-};
-module.exports.onLoad = () => {
+  };
+  module.exports.onLoad = () => {
     const fs = require("fs-extra");
     const request = require("request");
     const dirMaterial = __dirname + `/noprefix/`;
     if (!fs.existsSync(dirMaterial + "noprefix")) fs.mkdirSync(dirMaterial, { recursive: true });
     if (!fs.existsSync(dirMaterial + "hi.gif")) request("https://media.discordapp.net/attachments/849164098024374283/859645612097798184/received_373965544066156.gif").pipe(fs.createWriteStream(dirMaterial + "hi.gif"));
-}
-module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils, global, client }) => {
+  }
+  module.exports.handleEvent = async ({ event, api }) => {
     const fs = require("fs");
     let dt = await api.getUserInfo(event.senderID);
     let name = dt[event.senderID].name;
+  
+    var { threadID, messageID, body, senderID } = event;
+    if(senderID == api.getCurrentUserID()) return;
+    function out(data){
+        api.sendMessage(data, threadID, messageID)
+    }
+    //trả lời
     var msg = {
-				body: `Chào ${name}, chúc bạn một ngày mới tốt lành ❤️`,
-				attachment: fs.createReadStream(__dirname + `/noprefix/hi.gif`)
-			}
-    if (event.body.toLowerCase() == "hi"){
-        return api.sendMessage(msg,event.threadID,event.messageID);}
-    if (event.body.toLowerCase() == "hello"){
-        return api.sendMessage(msg,event.threadID,event.messageID);}
-    if (event.body.toLowerCase() == "chào"){
-        return api.sendMessage(msg,event.threadID,event.messageID);}
-    if (event.body.toLowerCase() == "hí"){
-        return api.sendMessage(msg,event.threadID,event.messageID);}
-    if (event.body.toLowerCase() == "lô"){
-        return api.sendMessage(msg,event.threadID,event.messageID);}
-        };
-module.exports.run = async ({ event, api, Currencies, args, utils }) => {
-return api.sendMessage("Dùng sai cách rồi lêu lêu",event.threadID)
-	}
+      body: `Chào ${name}, chúc bạn một ngày mới tốt lành ❤️`,
+      attachment: fs.createReadStream(__dirname + `/noprefix/hi.gif`)
+    }
+    // Gọi bot
+    var arr = ["hi", "Hi", "Hello", "hello", "lô", "hí lô"];
+    arr.forEach(i=> {
+        if(body == i) return out(msg)
+     });
+  
+  };
+  module.exports.run = async ({ event, api }) => {
+    return api.sendMessage("Dùng sai cách rồi lêu lêu", event.threadID)
+  }
+  
