ab40c8ddadfe9cd7a33e66880be5f0a95cd6f4fe
demngayyeu <nguyenmanhict@gmail.com>
2021-09-06 01:25:47 +0800

    del

--- includes/controllers/users.js
+++ includes/controllers/users.js
@@ -1,118 +1,49 @@
-module.exports = function ({ models, api }) {
-	const Users = models.use('Users');
-
-	async function getInfo(id) {
-		return (await api.getUserInfo(id))[id];
-	}
-
-	async function getNameUser(id) {
-		try {
-			if (global.data.userName.has(id)) return global.data.userName.get(id);
-			else if (global.data.allUserID.includes(id)) {
-				const nameUser = (await this.getData(id)).name;
-				if (nameUser) return nameUser;
-				else return "Người dùng facebook";
-			} else return "Người dùng facebook";
-		}
-		catch { return "Người dùng facebook" }
-	}
-
-	async function getNameUserData(id) {
-		try {
-			if (global.data.userName.has(id)) return global.data.userName.get(id);
-
-			var data;
-			data = await api.httpGet("https://www.facebook.com/profile.php?id=" + id, {});
-			if (data.includes('for (;;);{"redirect":"')) {
-				var { x } = JSON.parse(`{"x":"${data.toString().split('":"')[1].split('"}')[0]}"}`);
-				data = await api.httpGet(`https://www.facebook.com${x.split("?")[0]}`, {});
-			}
-			var name = data.toString().split("<title>")[1].split("</title>")[0];
-
-			if (name.toLocaleLowerCase().includes("facebook")) name = (await this.getInfo(id)).name; 
-			return name;
-		}
-		catch (e) {
-			logger(e, "error");
-			return "Người dùng facebook";
-		}
-	}
-
-	async function getAll(...data) {
-		var where, attributes;
-		for (const i of data) {
-			if (typeof i != 'object') throw global.getText("users", "needObjectOrArray");
-			if (Array.isArray(i)) attributes = i;
-			else where = i;
-		}
-		try {
-			return (await Users.findAll({ where, attributes })).map(e => e.get({ plain: true }));
-		}
-		catch (error) {
-			console.error(error);
-			throw new Error(error);
-		}
-	}
-
-	async function getData(userID) {
-		try {
-			const data = await Users.findOne({ where: { userID } });
-			if (data) return data.get({ plain: true });
-			else return false;
-		}
-		catch(error) {
-			console.error(error);
-			throw new Error(error);
-		}
-	}
-
-	async function setData(userID, options = {}) {
-		if (typeof options != 'object' && !Array.isArray(options)) throw global.getText("users", "needObject");
-		try {
-			(await Users.findOne({ where: { userID } })).update(options);
-			return true;
-		}
-		catch (error) {
-			try {
-				await this.createData(userID, options);
-			} catch (error) {
-				console.error(error);
-				throw new Error(error);
-			}
-		}
-	}
-
-	async function delData(userID) {
-		try {
-			(await Users.findOne({ where: { userID } })).destroy();
-			return true;
-		}
-		catch (error) {
-			console.error(error);
-			throw new Error(error);
-		}
-	}
-
-	async function createData(userID, defaults = {}) {
-		if (typeof defaults != 'object' && !Array.isArray(defaults)) throw global.getText("users", "needObject");
-		try {
-			await Users.findOrCreate({ where: { userID }, defaults });
-			return true;
-		}
-		catch (error) {
-			console.error(error);
-			throw new Error(error);
-		}
-	}
-
-	return {
-		getInfo,
-		getNameUser,
-		getNameUserData,
-		getAll,
-		getData,
-		setData,
-		delData,
-		createData
-	};
+module.exports.config = {
+    name: "fixspam-chuibot",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "ManhG",
+    description: "Người chửi bot sẽ tự động bị ban khỏi hệ thống <3",
+    commandCategory: "noprefix",
+    usages: "",
+    cooldowns: 0,
+    denpendencies: {}
 };
+
+module.exports.handleEvent = async({ event, api, Users }) => {
+    var { threadID, messageID, body, senderID, reason } = event;
+    const moment = require("moment-timezone");
+    const time = moment.tz("Asia/Ho_Chi_minh").format("HH:MM:ss L");
+    if (senderID == api.getCurrentUserID()) return;
+    let name = await Users.getNameUser(event.senderID);
+    //trả lời
+    var msg = {
+            body: `»Thông báo từ Admin«\n\n${name}, Bạn thật ngu ngok khi chửi bot vì vậy bot đã tự động ban bạn khỏi hệ thống\n\n💌Liên hệ Admin:\nhttps://facebook.com/manhict \nđể được gỡ ban bạn nhé \n\n🎭Thả tym cho bạn nè <3`
+        }
+        // Gọi bot
+    const arr = ["botngu", "Botngu", "Bot ngu", "bot ngu", "bot gà", "Bot gà", "con bot lol", "Con bot lol", "Bot ngu lol", "bot ngu lol", "Bot chó", "bot chó", "dm bot", "Dm bot", "Đm bot", "đm bot", "dmm bot", "Dmm bot", "dmm bot", "Dmm bot", "Đmm bot", "đmm bot", "đb bot", , "Đb bot", "bot điên", "Bot điên", "bot dở", "Bot dở", "Bot khùng", "bot khùng", "đĩ bot", "Đĩ bot", "Bot paylac rồi", "con bot lòn", "Con bot lòn", "cmm bot", "Cmm bot", "clap bot", "Clap bot", "bot ncc", "Bot ncc", "bot oc", "Bot oc", "bot óc", "Bot óc", "bot óc chó", "Bot óc chó", "cc bot", "Cc bot", "bot tiki", "Bot tiki", "Lozz bottt", "lol bot", "Lol bot", "Loz bot", "loz bot", "lồn bot", "Lồn bot", "bot lồn", "Bot lồn", "bot lon", "Bot lon", "Bot cac", "bot cac", "bot nhu lon", "Bot nhu lon", "bot như cc", "Bot như cc", "bot như bìu", "Bot như bìu", "Bot sida", "bot sida", "bot fake", "Bot fake", "mạnh ngu", "Mạnh ngu"];
+    arr.forEach(i => {
+        const uidUser = event.senderID;
+        if (body == i) {
+            modules = "chui bot:"
+            console.log(name, modules, i);
+            const data = Users.getData(uidUser).data || {};
+            Users.setData(uidUser, { data });
+            data.banned = 1;
+            data.reason = i || null;
+            data.dateAdded = time;
+            global.data.userBanned.set(uidUser, { reason: data.reason, dateAdded: data.dateAdded });
+
+            api.sendMessage(msg, threadID, () => {
+                const listAdmin = global.config.ADMINBOT;
+                var idad = listAdmin;
+                for (var idad of listAdmin) {
+                    api.sendMessage(`=== Bot Notification ===\n\n🆘Tội nhân: ${name}\n🔰Uid: ${uidUser}\n😥Chửi bot: ${i}\n\n Đã bị ban khỏi hệ thống`, idad);
+                }
+            })
+        }
+    });
+};
+module.exports.run = async({ event, api }) => {
+    return api.sendMessage("( \\_/)                                                                            ( •_•)                                                                            // >🧠                                                            Đưa não cho bạn lắp vào đầu nè.\nCó biết là lệnh Noprefix hay không?", event.threadID)
+}
