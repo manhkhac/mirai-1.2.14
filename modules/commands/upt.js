f80e534cbbf6ac5da6eca15739b6516083e682b5
demngayyeu <82906020+demngayyeu@users.noreply.github.com>
2021-10-04 03:48:44 +0800

    Create upt.js

---
+++ modules/commands/upt.js
@@ -0,0 +1,19 @@
+module.exports.config = {
+    name: "upt",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "ManhG",
+    description: "Kiểm tra thời gian bot đã online",
+    commandCategory: "system",
+    cooldowns: 5,
+    dependencies: {}
+};
+
+module.exports.run = async({ api, event }) => {
+    const time = process.uptime(),
+        hours = Math.floor(time / (60 * 60)),
+        minutes = Math.floor((time % (60 * 60)) / 60),
+        seconds = Math.floor(time % 60);
+
+    return api.sendMessage(`${hours} : ${minutes} : ${seconds}`, event.threadID, event.messageID);
+}
