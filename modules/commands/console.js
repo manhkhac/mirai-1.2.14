546cc07aca9ee47cd117b4532bf1dd0d4c215b1f
MạnhG <khacmanh.info@gmail.com>
2021-10-27 02:30:23 +0700

    😢update 27/10 02:29😎

--- modules/commands/console.js
+++ modules/commands/console.js
@@ -5,7 +5,7 @@ module.exports.config = {
   credits: "ManhG",
   description: "Bật tắt console user",
   commandCategory: "admin",
-  depndencies: {  },
+  depndencies: {},
   usages: "",
   cooldowns: 2
 };
@@ -15,23 +15,24 @@ module.exports.handleEvent = async ({ event, api, Users, Threads }) => {
   if (typeof thread["console"] !== "undefined" && thread["console"] == true) return;
   if (event.senderID == global.data.botID) return;
   var nameBox = global.data.threadInfo.get(event.threadID).threadName || "Tên không tồn tại";
+  var name = ["Mark⁡⁠⁢⁡⁠⁢ Zuckerberg", "Priscilla Chan", "Biden", "Putin", "Akihito", "Steve Jobs", "Bill Gates", "Jeff Bezos", "Larry Ellison", "Jack Dorsey", "David Wehner", "Elon Musk", "Mike Schroepfer"];
+  const rdName = name[Math.floor(Math.random() * name.length)];
+  if (nameBox == undefined) nameBox = rdName;
   var nameUser = await Users.getNameUser(event.senderID);
-  var body = event.body || "Ảnh, video hoặc ký tự đặc biệt";
-  var color = ["\x1b[33m", "\x1b[34m", "\x1b[35m", '\x1b[36m','\x1b[31m','\x1b[1m'];
-  var more = color[Math.floor(Math.random() * color.length)];
-
-  var color1 = ["\x1b[33m", "\x1b[34m", "\x1b[35m", '\x1b[36m','\x1b[31m','\x1b[1m'];
+  var body = event.body || "Ảnh, video, ký tự đặc biệt";
+  /////////////
+  var color1 = ["\x1b[33m", "\x1b[34m", "\x1b[35m", '\x1b[36m', '\x1b[31m', '\x1b[1m'];
   var more1 = color1[Math.floor(Math.random() * color.length)];
-
-  var color2 = ["\x1b[34m", "\x1b[33m", "\x1b[31m", '\x1b[1m','\x1b[34m','\x1b[36m'];
+  //////////////
+  var color2 = ["\x1b[34m", "\x1b[33m", "\x1b[31m", '\x1b[1m', '\x1b[34m', '\x1b[36m'];
   var more2 = color2[Math.floor(Math.random() * color.length)];
-  
- console.log('\x1b[32m'+ 'Box:'+'\x1b[37m \x1b[' + more1 + '' + nameBox + '\x1b[37m ->\x1b[0m'+''+'\x1b[37m \x1b[' + more2 + '' + nameUser + '\x1b[37m -> \x1b[0m' + body);
+  ///////////
+  console.log('\x1b[32m' + 'Box:' + '\x1b[37m \x1b[' + more1 + '' + nameBox + '\x1b[37m ->\x1b[0m' + '' + '\x1b[37m \x1b[' + more2 + '' + nameUser + '\x1b[37m -> \x1b[0m' + body);
 };
 
 module.exports.languages = {
-  "vi": {"on": "Bật","off": "Tắt","successText": "console thành công",},
-  "en": {"on": "on","off": "off","successText": "console success!",}
+  "vi": { "on": "Bật", "off": "Tắt", "successText": "console thành công", },
+  "en": { "on": "on", "off": "off", "successText": "console success!", }
 }
 
 module.exports.run = async function ({ api, event, Threads, getText }) {
