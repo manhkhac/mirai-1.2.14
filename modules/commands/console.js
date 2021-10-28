10fc88fe9a0fe648849981e19c12318be06a19cb
MạnhG <41892269+manhkhac@users.noreply.github.com>
2021-10-28 09:50:16 +0700

    Update console.js

--- modules/commands/console.js
+++ modules/commands/console.js
@@ -14,28 +14,14 @@ module.exports.handleEvent = async ({ event, api, Users, Threads }) => {
   if (event.senderID == global.data.botID) return;
   var dataBox = global.data.threadInfo.get(event.threadID);
   var name = ["Mark⁡⁠⁢⁡⁠⁢ Zuckerberg", "Priscilla Chan", "Biden", "Putin", "Akihito", "Steve Jobs", "Bill Gates", "Jeff Bezos", "Larry Ellison", "Jack Dorsey", "David Wehner", "Elon Musk", "Mike Schroepfer"];
-  const threadName = name[Math.floor(Math.random() * name.length)];
-  if (undefined) {
-     nameBox = threadName;
-  }
-  else nameBox = dataBox.threadName;
+  const rdThread = name[Math.floor(Math.random() * name.length)];
+  var nameBox = dataBox.threadName || rdThread;
 
   var nameUser = await Users.getNameUser(event.senderID);
   var body = event.body || "Ảnh, video, ký tự đặc biệt";
-  /////////////
-  var color1 = ["\x1b[33m", "\x1b[34m", "\x1b[35m", '\x1b[36m', '\x1b[31m', '\x1b[1m'];
-  var more1 = color1[Math.floor(Math.random() * color1.length)];
-  //////////////
-  var color2 = ["\x1b[34m", "\x1b[33m", "\x1b[31m", '\x1b[1m', '\x1b[34m', '\x1b[36m'];
-  var more2 = color2[Math.floor(Math.random() * color2.length)];
   ///////////
-  console.log('\x1b[32m' + 'Box:' + '\x1b[37m \x1b[' + more1 + '' + nameBox + '\x1b[37m ->\x1b[0m' + '' + '\x1b[37m \x1b[' + more2 + '' + nameUser + '\x1b[37m -> \x1b[0m' + body);
+  console.log('Box: ' + '' + nameBox + '->' + nameUser + '->' + body);
 };
-
-module.exports.languages = {
-  "vi": { "on": "Bật", "off": "Tắt", "successText": "console thành công", },
-  "en": { "on": "on", "off": "off", "successText": "console success!", }
-}
 
 module.exports.run = async function ({ api, event, Threads, getText }) {
   if ((this.config.credits) != "ManhG") { return api.sendMessage(`⚡Phát hiện credits đã bị thay đổi`, event.threadID, event.messageID)}
