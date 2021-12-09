f31c635e125311a8e5b89ea1b9838b63f6e9b668
MạnhG <41892269+manhkhac@users.noreply.github.com>
2021-12-09 22:32:19 +0700

    Update setup.js

--- modules/events/setup.js
+++ modules/events/setup.js
@@ -17,20 +17,20 @@ module.exports.onLoad = async function() {
     ///////////////join Noti start
     const pathJoin = resolve(__dirname, "cache/joinNoti");
     if (!existsSync(pathJoin)) mkdirSync(pathJoin, { recursive: true });
-    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join2.mp4'))) await downloadFile("https://github.com/manhkhac/mirai-1.2.8/raw/data/mp4/join.mp4", resolve(__dirname, 'cache/joinNoti', 'join2.mp4'));
-    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join1.mp4'))) await downloadFile("https://github.com/manhkhac/mirai-1.2.8/raw/data/mp4/joinNoti.mp4", resolve(__dirname, 'cache/joinNoti', 'join1.mp4'));
+    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join2.mp4'))) await downloadFile("https://manhkhac.github.io/data/mp4/join.mp4", resolve(__dirname, 'cache/joinNoti', 'join2.mp4'));
+    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join1.mp4'))) await downloadFile("https://manhkhac.github.io/data/mp4/joinNoti.mp4", resolve(__dirname, 'cache/joinNoti', 'join1.mp4'));
 
-    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join3.mp4'))) await downloadFile("https://github.com/manhkhac/mirai-1.2.8/raw/data/mp4/join3.mp4", resolve(__dirname, 'cache/joinNoti', 'join3.mp4'));
-    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join4.mp4'))) await downloadFile("https://github.com/manhkhac/mirai-1.2.8/raw/data/mp4/join4.mp4", resolve(__dirname, 'cache/joinNoti', 'join4.mp4'));
-    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join5.mp4'))) await downloadFile("https://github.com/manhkhac/mirai-1.2.8/raw/data/mp4/join5.mp4", resolve(__dirname, 'cache/joinNoti', 'join5.mp4'));
+    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join3.mp4'))) await downloadFile("https://manhkhac.github.io/data/mp4/join3.mp4", resolve(__dirname, 'cache/joinNoti', 'join3.mp4'));
+    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join4.mp4'))) await downloadFile("https://manhkhac.github.io/data/mp4/join4.mp4", resolve(__dirname, 'cache/joinNoti', 'join4.mp4'));
+    if (!existsSync(resolve(__dirname, 'cache/joinNoti', 'join5.mp4'))) await downloadFile("https://manhkhac.github.io/data/mp4/join5.mp4", resolve(__dirname, 'cache/joinNoti', 'join5.mp4'));
     ///////////////join Noti end
 
     ///////////////leave Noti start
     const pathLeave = resolve(__dirname, "cache/leaveNoti");
     if (!existsSync(pathLeave)) mkdirSync(pathLeave, { recursive: true });
-    if (!existsSync(resolve(__dirname, 'cache/leaveNoti', 'leave1.gif'))) await downloadFile("https://github.com/manhkhac/mirai-1.2.8/raw/data/gif/leave.gif", resolve(__dirname, 'cache/leaveNoti', 'leave1.gif'));
-    if (!existsSync(resolve(__dirname, 'cache/leaveNoti', 'leave2.gif'))) await downloadFile("https://raw.githubusercontent.com/manhkhac/mirai-1.2.8/data/gif/byebye.gif", resolve(__dirname, 'cache/leaveNoti', 'leave2.gif'));
-    if (!existsSync(resolve(__dirname, 'cache/leaveNoti', 'leave3.gif'))) await downloadFile("https://raw.githubusercontent.com/manhkhac/mirai-1.2.8/data/gif/leave3.gif", resolve(__dirname, 'cache/leaveNoti', 'leave3.gif'));
+    if (!existsSync(resolve(__dirname, 'cache/leaveNoti', 'leave1.gif'))) await downloadFile("https://manhkhac.github.io/data/gif/leave.gif", resolve(__dirname, 'cache/leaveNoti', 'leave1.gif'));
+    if (!existsSync(resolve(__dirname, 'cache/leaveNoti', 'leave2.gif'))) await downloadFile("https://manhkhac.github.io/data/gif/byebye.gif", resolve(__dirname, 'cache/leaveNoti', 'leave2.gif'));
+    if (!existsSync(resolve(__dirname, 'cache/leaveNoti', 'leave3.gif'))) await downloadFile("https://manhkhac.github.io/data/gif/leave3.gif", resolve(__dirname, 'cache/leaveNoti', 'leave3.gif'));
     ///////////////leave Noti end
 
     return;
